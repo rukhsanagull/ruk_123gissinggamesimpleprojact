f32e2004f18ddcb8ef8e902abb748ffc7c2d6276
rukhsanagull <164994220+rukhsanagull@users.noreply.github.com>
2024-06-14 15:47:06 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+#!/user/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const rendomNumber = Math.floor(Math.random() * 10 + 1);
+let answer = await inquirer.prompt([
+    {
+        name: "gissingGame",
+        type: "input",
+        message: `Guess a number between 1 and 10`
+    }
+]);
+if (answer.gissingGame == rendomNumber) {
+    console.log(chalk.green("Congratulations! You guessed the number correctly!"));
+}
+else {
+    console.log("please try agine");
+}
